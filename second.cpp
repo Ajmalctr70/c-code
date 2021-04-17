21048bac20548e5cd556701fc4da271fd597895f
Ajmalctr70 <82159742+Ajmalctr70@users.noreply.github.com>
2021-04-17 15:56:24 +0530

    Add files via upload
    completed

---
+++ second.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int a , b , c;
+	
+	cout<<"enter number\n";
+	cin>>a;
+	cout<<"enter number\n";
+	cin>>b;
+	cout<<"enter number\n";
+	cin>>c;
+	
+	if (a>b){
+		if (a>c){
+			cout<<"a is the largest number";
+		}
+		else {
+			cout<<"c is the largest number";}}
+else (b>a);{
+	if(b>c){
+		cout<<"b is the largest number";
+	}
+	else {
+		cout<<"c is the largest number";}}}
